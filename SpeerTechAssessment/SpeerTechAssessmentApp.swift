e014a08f9ee95eb36407e2c80edfe40dad3d5383
Brian Ortiz <mateo972000@gmail.com>
2023-06-05 16:07:31 -0400

    Initial Commit

---
+++ SpeerTechAssessment/SpeerTechAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeerTechAssessmentApp.swift
+//  SpeerTechAssessment
+//
+//  Created by Brian Ortiz on 2023-06-05.
+//
+
+import SwiftUI
+
+@main
+struct SpeerTechAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
